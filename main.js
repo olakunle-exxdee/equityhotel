64a7fde61b5f9ff311d1b0e16f08586aa65dfc84
olakunle_exdee <saheedogunsolu2016@gmail.com>
2020-10-09 01:50:14 +0100

    header done

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector(".hamburger ");
 const menu = document.querySelector(" .menu");
+const logos = document.querySelector(" .logos");
 const nav2 = document.querySelector(".navbar__content2");
 let showMenu = false;
 
@@ -7,10 +8,12 @@ hamburger.addEventListener("click", () => {
   if (!showMenu) {
     nav2.classList.add("open");
     hamburger.classList.add("open");
+    logos.classList.add("open");
     showMenu = true;
   } else {
     nav2.classList.remove("open");
     hamburger.classList.remove("open");
+     logos.classList.remove("open");
     showMenu = false;
   }
 });
